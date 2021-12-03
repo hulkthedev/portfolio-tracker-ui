4d3ca0f531ee1f3704439e7fdd23e77f0d99ba26
albe <a.beirith@wertgarantie.com>
2021-12-03 13:52:28 +0100

    add layout

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../global.css'
+
+export default function Portfolio({ Component, pageProps }) {
+    // Use the layout defined at the page level, if available
+    const getLayout = Component.getLayout || ((page) => page)
+
+    return getLayout(<Component {...pageProps} />)
+}
